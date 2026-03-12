2c035dcca6782b8bc2a6ff4e44253546c9a82490
Gabriel Gavrilov <gabriel.gavrilov02@gmail.com>
2026-03-12 08:29:31 -0600

    git config test

--- Kumi.API/Controllers/TestController.cs
+++ Kumi.API/Controllers/TestController.cs
@@ -6,7 +6,9 @@ namespace Kumi.API.Controllers
     public class TestController : BaseApiController
     {
         [HttpGet]
-        public async Task<ActionResult<string>> Test() =>
-            HandleResult(Result<string>.Success("And we are off!"));
+        public async Task<ActionResult<string>> Test()
+        {
+            return HandleResult(Result<string>.Success("And we are off!"));
+        }
     }
 }
