49540d067f9160feb01d2d68ee1bd318d9164150
Gabriel Gavrilov <gabriel.gavrilov02@gmail.com>
2026-03-15 11:17:49 -0600

    Update

--- Kumi.Domain/Message.cs
+++ Kumi.Domain/Message.cs
@@ -1,9 +1,12 @@
 using System;
+using System.Text.Json.Serialization;
 
 namespace Kumi.Domain;
 
 public class Message
 {
+    [JsonPropertyName("role")]
     public string? Role { get; set; }
+    [JsonPropertyName("content")]
     public string? Content { get; set; }
 }
--- Kumi.LLM/Integrations/Ollama/OllamaResponse.cs
+++ Kumi.LLM/Integrations/Ollama/OllamaResponse.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Text.Json.Serialization;
 using Kumi.Domain;
 
 namespace Kumi.LLM.Integrations.Ollama;
 
 public class OllamaResponse
 {
+    [JsonPropertyName("message")]
     public Message? Message { get; set; }
 }
