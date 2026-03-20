c2d0dca31a09385184457ff7216767bc74157277
Gabriel Gavrilov <gabriel.gavrilov02@gmail.com>
2026-03-20 07:46:27 -0600

    Added assemblers

---
+++ Kumi.API/Assemblers/ParameterAssembler.cs
@@ -0,0 +1,29 @@
+using System;
+using Kumi.API.DTOs;
+using Kumi.Domain;
+
+namespace Kumi.API.Assemblers;
+
+public class ParameterAssembler
+{
+    public ParameterDto Assemble(Parameter entity)
+    {
+       return new ParameterDto
+       {
+          Type = entity.Type;
+          Description = entity.Description;
+          Required = entity.Required;
+       }
+    }
+
+    public Parameter Disassemble(ParameterDto dto)
+    {
+        return new Parameter
+        {
+            Type = dto.Type;
+            Description = dto.Description;
+            Required = dto.Required;
+        }
+    }
+
+}
--- Kumi.API/Assemblers/ToolAssembler.cs
+++ Kumi.API/Assemblers/ToolAssembler.cs
@@ -1,8 +1,35 @@
 using System;
+using Kumi.API.DTOs;
+using Kumi.Domain.Tools;
 
-namespace Kumi.API.DTOs;
+namespace Kumi.API.Assemblers;
 
 public class ToolAssembler
 {
+  
+    public ToolDto Assemble(Tool entity) 
+    {
+        return new ToolDto 
+        {
+            ToolId = entity.ToolId;
+            Url = entity.Url;
+            Method = entity.Method;
+            Name = entity.Name;
+            Description = entity.Description;
+            Parameters = entity.Parameters;
+        }
+    }
+
+    public Tool Disassemble(ToolDto dto)
+    {
+        return new Tool
+        {
+           Url = dto.Url;
+           Method = dto.Method;
+           Name = dto.Name;
+           Description = dto.Description;
+           Parameters = dto.Parameters;
+        }
+    }
 
 }
