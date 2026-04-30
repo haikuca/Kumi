01d475e71e56cf6effea1858948c064d78e9ed80
Gabriel Gavrilov <gabriel.gavrilov02@gmail.com>
2026-04-30 09:38:04 -0600

    Update

--- Kumi.UI/src/hooks/useToolActions.ts
+++ Kumi.UI/src/hooks/useToolActions.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import agent from "../lib/api/Agent";
 import type { Tool } from "../lib/types/Tool";
 
@@ -8,6 +8,21 @@ export function useTools() {
     queryFn: async () => {
       const response = await agent.get<Tool[]>("/tools")
       return response.data;
+    }
+  })
+}
+
+export function useCreateTool() {
+  const client = useQueryClient();
+  return useMutation({
+    mutationFn: async (tool: Tool) => {
+      const response = await agent.post("/tools", tool)
+      return response.data;
+    },
+    onSuccess: async () => {
+      await client.invalidateQueries({
+        queryKey: ['tools']
+      })
     }
   })
 }
